4c48264aeea62ddf9a551d1488ccfe732bbde9ea
Higor <weshigor@gmail.com>
2022-08-18 13:33:17 -0300

    Sbindo dois arquivos que nao pertencem a esta pasta para upar diretorio no github

---
+++ 3-Python-introducao-POO/comentarios-de-codigo-python.py
@@ -0,0 +1,13 @@
+# Isso é um comentário e é usado a cerquilha para comentar
+"""3 aspas seja simples ou duplas são usadas para documentar"""
+
+print('Linha 1')
+print('Linha 2')
+# Falando do que se trata essa linha 
+print('Linha 3') 
+'''
+print('Linha 4') 
+print('Linha 5')
+print('Linha 6')
+print('Linha 7')'''
+print('Linha 8')
