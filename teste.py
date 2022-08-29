54d6fa4babd28edfc7d28172ff0e9d059a5c6d95
Higor <weshigor@gmail.com>
2022-08-29 11:53:32 -0300

    Subindo atualizações no arquivo teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,11 @@
-print("hello world")
+print('***************************')
+print('Informe seu primeiro nome:')
+nome = str(input('>> '))
+print('***************************')
+print('Informe uma senha de cadastro (APENAS NÚMEROS):')
+password = int(input('>> '))
+print('***************************')
+
+
+print(f'Bem vindo {nome}!! Senha cadastrada com sucesso!')
+
